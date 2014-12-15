d77d4b05adbd707a09395969e0c8f804454724a5
Chris Fordham <chris.fordham@industrieit.com>
2014-12-15 22:09:58 +1100

    Add initial files added from meez init.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# Encoding: utf-8
+name             'gitlab-omnibus'
+maintainer       'Chris Fordham'
+maintainer_email 'chris@fordham-nagy.id.au'
+license          'Apache 2.0'
+description      'Installs/Configures gitlab-omnibus'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
