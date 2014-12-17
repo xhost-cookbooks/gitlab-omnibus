fd75032396a8a1c37ae49b08dc334e760bf9994e
Chris Fordham <chris@xhost.com.au>
2014-12-17 17:27:34 +1100

    Add in more attributes to metadata.

--- metadata.rb
+++ metadata.rb
@@ -16,10 +16,67 @@ attribute 'gitlab-omnibus/package/download_url',
           display_name: 'GitLab Omnibus Package URL',
           description: 'URL to fetch the GitLab Omnibus package from.',
           default: 'https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.5.3-omnibus.5.2.1.ci-1_amd64.deb',
-          recipes: ['roundcube::default', 'roundcube::install']
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install']
 
 attribute 'gitlab-omnibus/package/checksum',
           display_name: 'GitLab Omnibus Package Checksum',
           description: 'The SHA256 checksume of the GitLab Omnibus package.',
           default: 'c00a73664aa197f78a4df887fd020ea686e299d3cb0ea24f25dcc18fea971902',
-          recipes: ['roundcube::default', 'roundcube::install']
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install']
+
+attribute 'gitlab-omnibus/external_url',
+          display_name: 'GitLab Omnibus External URL',
+          description: 'The external or public URL of the GitLab web interface.',
+          default: false,
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
+
+attribute 'gitlab-omnibus/enable_tls',
+          display_name: 'GitLab Omnibus Enable TLS',
+          description: 'Whether to enable TLS (SSL) support.',
+          default: false,
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
+
+attribute 'gitlab-omnibus/nginx/redirect_http_to_https',
+          display_name: 'GitLab Omnibus Redirect HTTP->HTTPS',
+          description: 'Whether to redirect HTTP requests to HTTPS.',
+          default: false,
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
+
+attribute 'gitlab-omnibus/nginx/ssl_certificate',
+          display_name: 'GitLab Omnibus SSL Certificate',
+          description: 'The x509 (pem) TLS/SSL certificate.',
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
+
+attribute 'gitlab-omnibus/nginx/ssl_certficiate_key',
+          display_name: 'GitLab Omnibus SSL Certificate Key',
+          description: "The x509 (pem) TLS/SSL certificate's key.",
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
+
+attribute 'gitlab-omnibus/rails/ldap_enabled',
+          display_name: 'GitLab Omnibus LDAP Enabled',
+          description: 'Whether to enable LDAP with GitLab.',
+          default: false,
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
+
+attribute 'gitlab-omnibus/rails/gitlab_email_from',
+          display_name: 'GitLab Omnibus Email From',
+          description: 'The email address mail is sent From: for GitLab.',
+          default: 'gitlab@example.com',
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
+
+attribute 'gitlab-omnibus/rails/ldap_group_base',
+          display_name: 'GitLab Omnibus LDAP Group Base',
+          description: 'The LDAP group base.',
+          default: '',
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
+
+attribute 'gitlab-omnibus/rails/ldap_user_filter',
+          display_name: 'GitLab Omnibus LDAP User Filter',
+          description: 'The LDAP user filter.',
+          default: '',
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
+
+attribute 'gitlab-omnibus/rails/ldap_servers_yaml',
+          display_name: 'GitLab Omnibus LDAP Servers',
+          description: 'The LDAP servers provided in YAML format.',
+          recipes: ['gitlab-omnibus::default', 'gitlab-omnibus::install', 'gitlab-omnibus::configure']
