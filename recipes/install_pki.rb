28f9b8bc71a79a12fe667f97fa7f76bdac7e74ff
Chris Fordham <chris@xhost.com.au>
2014-12-23 16:56:07 +1100

    Add initial install_pki recipe.

---
+++ recipes/install_pki.rb
@@ -0,0 +1,40 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: gitlab-omnibus
+# Recipe:: install_pki
+#
+# Copyright 2014, Chris Fordham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'uri'
+
+# install ssl cert/key
+template "/etc/gitlab/ssl/#{URI(node['gitlab-omnibus']['external_url']).host}-cert.pem" do
+  variables(
+    cert_content: node['gitlab-omnibus']['ssl']['certificate']
+  )
+  mode 0400
+  notifies :run, 'execute[gitlab-ctl reconfigure]', :delayed
+  only_if { node['gitlab-omnibus']['enable_tls'] }
+end
+
+template "/etc/gitlab/ssl/#{URI(node['gitlab-omnibus']['external_url']).host}-key.pem" do
+  variables(
+    key_content: node['gitlab-omnibus']['ssl']['certificate_key']
+  )
+  mode 0400
+  notifies :run, 'execute[gitlab-ctl reconfigure]', :delayed
+  only_if { node['gitlab-omnibus']['enable_tls'] }
+end
